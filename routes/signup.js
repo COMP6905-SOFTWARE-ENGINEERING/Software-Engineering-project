47427d9dea452b90e6e254d8691ae7819fb70434
liaoyuhao <liaoyuhao@alibaba-inc.com>
2018-11-06 03:53:17 +0800

    add signup page and basic routing function

---
+++ routes/signup.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var crypto = require('crypto');
+var router = express.Router();
+// var accMgmtModel = require('../models/account_db.js');
+
+router.get('/', function(req, res, next) {
+	res.render('signup', {title: 'register page'});
+});
+
+router.post('/',function(req, res){
+	var md5 = crypto.createHash('md5');
+	var password = md5.update(req.body.password).digest('hex');
+	if(req.body.usertype == 'person'){
+		var registerData = {
+			username: req.body.username,
+			password: password,
+			email: req.body.email,
+			realname: req.body.realname,
+			IDnumber: req.body.IDnumber,
+			usertype: req.body.usertype
+		};
+	}else{
+		var registerData = {
+			username: req.body.username,
+			password: password,
+			email: req.body.email,
+			companyname: req.body.companyname,
+			companytype: req.body.companytype,
+			address: req.body.address,
+			representative: req.body.representative,
+			usertype: req.body.usertype
+		};
+	}
+	accMgmtModel.register(registerData, function(status){
+		if (status == 'ok'){
+			if (registerData.usertype == 'person'){
+				req.session.user = {
+					username: registerData.username,
+					usertype: registerData.usertype
+				};
+				res.json({status:status, flag:1});
+			}else{
+				res.json({status:status, flag:3});
+			}
+		}else if (status.code == 11000){
+			if (registerData.usertype == 'person'){
+				if (status.errmsg.indexOf('username') > 0){
+					res.json({status:'用户名已存在', flag:0, errKey: 'username'});
+				}else if (status.errmsg.indexOf('email') > 0){
+					res.json({status:'邮箱已存在', flag:0, errKey: 'email'});
+				}else if (status.errmsg.indexOf('IDnumber') > 0){
+					res.json({status:'身份证号已存在', flag:0, errKey: 'IDnumber'});
+				}
+			}else {
+				if (status.errmsg.indexOf('username') > 0){
+					res.json({status:'用户名已存在', flag:0, errKey: 'username'});
+				}else if (status.errmsg.indexOf('email') > 0){
+					res.json({status:'邮箱已存在', flag:0, errKey: 'email'});
+				}else if (status.errmsg.indexOf('companyname') > 0){
+					res.json({status:'公司名已存在', flag:0, errKey: 'companyname'});
+				}
+			}
+		}else {
+			res.json({status:status, flag:2});
+		}
+	});
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,13 +2,32 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var signupRouter = require('./routes/signup');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
  // send our index.html file to the user for the home page
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('mycookie'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/signup', signupRouter);
+
 app.get('/', function(req, res) {
 res.sendFile(path.join(__dirname + '/index.html'));
  });
-
+//app.get('/signup',function(req,res){
+//  res.sendFile(path.join(__dirname+'/signup.html'));
+//});
  // start the server
  app.listen(3334);
  console.log('3334 is the magic port!');
+console.log(__dirname + '/index.html');
