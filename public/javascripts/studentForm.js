fe5dfcd40649072b518c4365e984f74a610fa293
Mahesh Pochamreddy <39742295+kpochamreddy@users.noreply.github.com>
2018-11-13 16:47:02 -0330

    Adding Student profile page and its js

---
+++ public/javascripts/studentForm.js
@@ -0,0 +1,21 @@
+function edu() {
+    $("#educationFieldsWrapper .educationFields").clone().appendTo($("#educationFieldsWrapper "));
+
+
+}
+function skil() {
+    $("#skillsFieldsWrapper .skillFields").clone().appendTo($("#skillsFieldsWrapper "));
+
+
+}
+function research() {
+    $("#researchFieldsWrapper .researchFields").clone().appendTo($("#researchFieldsWrapper "));
+
+
+}
+function exp() {
+    $("#expFieldsWrapper .expFields").clone().appendTo($("#expFieldsWrapper "));
+
+
+}
+
