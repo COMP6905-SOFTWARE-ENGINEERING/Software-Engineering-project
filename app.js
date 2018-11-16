0d95946d861290e7a9bd9022801bae7e7e879a6e
olozanoperez <olozanoperez@mun.ca>
2018-11-16 16:03:23 -0330

    Changes to the App.js file to remove warnings

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var projectRouter = require('./routes/project');
 var app = express();
 
 var url = 'mongodb://127.0.0.1:27017/GradRecDB';
-mongoose.connect(url);
+mongoose.connect(url, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,7 +59,7 @@ app.use(function (req, res) {
 //});
  // start the server
  app.listen(3334);
- console.log('3334 is the magic port!');
+ console.log('Software Engineering Group 4 Port 3334 is now active');
 // console.log(__dirname + '/index.html');
 
 module.exports = app;
