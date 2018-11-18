79a0d4b985aaed34fe96d0c275e73b1a765b67a3
liaoyuhao <342129019@qq.com>
2018-11-19 07:50:23 +0800

    modify some fields in schema

--- models/project_db.js
+++ models/project_db.js
@@ -12,7 +12,7 @@ var projectSchema = new Schema({
         unique:true,
         select:true,
         required:[true, 'project name cannot be empty'],
-        match:[/^[a-zA-Z](\w)*$/, '用户名须以字母开始，且只能包含字母数字下划线']
+        match:[/^[a-zA-Z](\w)*$/, 'project name should start with alphabet and contain only alphabet and underline']
     },
     project_description:{
         type:String,
@@ -23,8 +23,8 @@ var projectSchema = new Schema({
     funding:{
         type:Number,
         required:[true, 'please input funding'],
-        min:[0, '请确认类型无误'],
-        max:[50000, '请确认类型无误']
+        min:[0, 'please enter a value between 0 and 50000'],
+        max:[50000, 'please enter a value between 0 and 50000']
     },
     required_skills: [{type:Schema.Types.ObjectId, ref: 'skill'}],
     start_date:{
@@ -40,7 +40,7 @@ var projectSchema = new Schema({
 var projectModel = mongoose.model('project', projectSchema);
 
 exports.listByOwner = function(reqData, callback){
-    projectModel.find(reqData, ['_id', 'project_name', 'project_description', 'funding', 'start_date', 'application_deadline'], {sort:{_id: 1}}, function(err, data){
+    projectModel.find(reqData, ['_id', 'isDefault', 'isPublic', 'profilename', 'realname', 'deliverer'], {sort:{_id: 1}}, function(err, data){
         if (err){
             callback(err, null);
         }else {
@@ -49,126 +49,24 @@ exports.listByOwner = function(reqData, callback){
     });
 };
 
-exports.projectLogin = function(reqData, callback){
-    projectModel.findOne({username:reqData.username, isApproved:true}, function(err, data){
+exports.createProfile = function(reqData, callback){
+    var Data = reqData;
+    projectModel.create(Data, function(err, data){
         if (err){
             callback(err);
-        }else if (data != null){
-            if (data.password == reqData.password){
-                callback('ok');
-            }else{
-                callback('密码错误');
-            }
-        }else{
-            callback('用户不存在或未审核');
+        }else {
+            callback('ok');
         }
     });
 };
 
-exports.projectRegister = function(reqData, callback){
-    projectModel.create({
-        username:reqData.username,
-        password:reqData.password,
-        email: reqData.email,
-        projectname: reqData.projectname,
-        projecttype: reqData.projecttype,
-        representative: reqData.representative,
-        address: reqData.address,
-    }, function(err, data){
+exports.findById = function(reqData, callback){
+    projectModel.findById(reqData._id, function(err, data){
         if(err){
-            callback(err);
-        }else{
-            callback('ok');
-        }
-    });
-};
-
-exports.projectCfmEmail = function(reqData, callback){
-    projectModel.findOne({username:reqData.username}, function(err, data){
-        if (err){
-            callback(err);
-        }else if (data != null){
-            if (data.email == reqData.email_confirm){
-                callback('ok');
-            }else{
-                callback('邮箱错误');
-            }
-        }else{
-            callback('用户不存在');
-        }
-    });
-};
-
-exports.projectChgPwd = function(reqData, callback){
-    projectModel.update({username:reqData.username}, {$set: {password: reqData.password}}, {runValidators: true}, function(err, data){
-        if (err){
-            callback(err);
-        }else {
-            callback('ok');
-        }
-    });
-};
-
-exports.projectModInfo = function(reqData, callback){
-    projectModel.update({username:reqData.username}, {$set: reqData}, {runValidators: true}, function(err, data){
-        if (err){
-            callback(err);
-        }else {
-            callback('ok');
-        }
-    });
-};
-
-exports.projectAccInfo = function(reqData, callback){
-    projectModel.findOne({username:reqData.username}, function(err, data){
-        if (err){
-            // err.err = 'err';
-            callback(err, null);
-        }else {
-            // data.err = 'ok';
-            callback('ok', data);
-        }
-    });
-};
-
-exports.adminApprove = function(reqData, callback){
-    projectModel.update({
-        username:reqData.username,
-    },{
-        $set:{isApproved: true}
-    }, function(err, data){
-        if(err){
-            callback(err);
-        }else {
-            callback('ok');
-        }
-    });
-};
-
-exports.adminReject = function(reqData, callback){
-    projectModel.remove({
-        username:reqData.username,
-        isApproved:false,
-    }, function(err, data){
-        if(err){
-            callback(err);
-        }else {
-            callback('ok');
-        }
-    });
-};
-
-exports.getRegList = function(reqData, callback){
-    projectModel.find({
-        isApproved:reqData.isApproved,
-    }, {
-        'password':0,
-        'email':0,
-    }, function(err, data){
-        if (err){
             callback(err, null);
         }else {
             callback(null, data);
         }
     });
 };
+
