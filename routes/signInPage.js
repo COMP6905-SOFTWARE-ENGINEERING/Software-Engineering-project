9e8b971452cd7510843a5b424c280a59a92ff73b
olozanoperez <olozanoperez@mun.ca>
2018-11-18 22:38:30 -0330

    Linked new Frontend with backend

---
+++ routes/signInPage.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var crypto = require('crypto');
+var router = express.Router();
+var accMgmtModel = require('../models/account_db.js');
+// var asdModel = require('../models/others.js');
+
+// router.get('/asd', function(req,res){
+// 	asdModel.asd('020000', function(data){
+// 		console.log(data);
+// 		res.json(data);
+// 	});
+// });
+
+// router.get('/fgh', function(req,res){
+// 	asdModel.fgh('020000', function(data){
+// 		console.log(data);
+// 		res.json(data);
+// 	});
+// });
+
+
+router.get('/', function(req, res, next) {
+    if (req.session.user){
+        res.redirect('/index');
+    }else{
+        res.render('login', {title: 'Login Panel'});
+    }
+});
+
+router.post('/',function(req,res){
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('hex');
+    var loginData = {
+        email: req.body.email,
+        password: password,
+        usertype: req.body.usertype
+    };
+    // var usertype = req.body.usertype;
+    accMgmtModel.login(loginData, function(status){
+        if (status == 'ok'){
+            // console.log('1');
+            req.session.user = {
+                firstname: loginData.username,
+                usertype: loginData.usertype
+            };
+            res.json({status:status, flag:1});
+        }else{
+            res.json({status:status, flag:0});
+        }
+    });
+    // res.json('ok');
+    // res.render('login', {flag: 1, title: 'Login Panel'});
+});
+
+
+
+module.exports = router;
