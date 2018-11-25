8f32ae37a02be703e54f234ecb14fcd2a1af0376
olozanoperez <olozanoperez@mun.ca>
2018-11-25 19:42:05 -0330

    load countries in users.js

--- models/area_db.js
+++ models/area_db.js
@@ -6,13 +6,15 @@ var areaSchema = new Schema({
     area: String,
 });
 
-var areaModel = mongoose.model('areas', areaSchema);
+var areaModel = mongoose.model('programs', areaSchema);
 
 exports.findAll = function(callback){
     areaModel.find({}, ['area'], function(err, data){
         if(err){
+            console.log("there was an error");
             callback(err, null);
         }else {
+            console.log("no error");
             callback('ok', data);
         }
     });
