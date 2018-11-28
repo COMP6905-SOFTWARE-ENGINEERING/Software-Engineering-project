d04d257fd700416f554ccabb8f06e1272b6328ec
Mahesh Pochamreddy <39742295+kpochamreddy@users.noreply.github.com>
2018-11-27 22:47:32 -0330

    Adding changes

--- public/javascripts/jquery_manager_match.js
+++ public/javascripts/jquery_manager_match.js
@@ -1,4 +1,11 @@
-acceptOffer()
-{
-    alert()
-}
+// function submit()
+// {
+// var table = $("#div_func");
+// var value_check = "";
+// for (var i = 1; i < table.rows.length; i++) {
+//     if ($('#chk')[i].is(':checked')) {
+//         value_check += i + ": " + $('#chk')[i].val();
+//     }
+// }
+// alert(value_check);
+// }
