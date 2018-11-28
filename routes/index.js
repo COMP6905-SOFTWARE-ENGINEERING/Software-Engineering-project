2f1975358b7b703af8f7fd1b984a10cb3f703f6c
olozanoperez <olozanoperez@mun.ca>
2018-11-27 23:28:47 -0330

    logout updated

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,7 @@ router.get('/cleanSession', function(req, res, next) {
         }else{
             res.clearCookie('mycookie');
             res.json({ret_code: 0, ret_msg: 'logout successfully'});
+            req.render('starting');
         }
     });
 });
